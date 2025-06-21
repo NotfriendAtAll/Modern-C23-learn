a5b5af0d9cca3daa855e1afed6b843b3952bdc1e
NotfriendAtAll <1976909647@qq.com>
2025-06-21 15:59:34 +0800

    fix some bug

--- src/src-2/modern_c.c
+++ src/src-2/modern_c.c
@@ -19,7 +19,7 @@ auto res=INT_MIN < -INT_MAX;
     printf("res:%d\n",res);
 double E[] = { [3] = 42.0, [2] = 37.0 }; // 类型为 double[4]
 double F[] = { 22.0, 17.0, 1, 0.5 };     // 类型为 double[4]
-printf("%d ",sizeof(E) / sizeof(E[0])); // 输出 4
-printf("%d ",sizeof(F)); // 输出 8,退化成指针
+printf("%zu ",sizeof(E) / sizeof(E[0])); // 输出 4
+printf("%zu ",sizeof(F)); // 输出 8,退化成指针
   return 0;
 }
